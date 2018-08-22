1a5d754ca0a4c20899b97e973cc1af8740445bd7
Suraj Kanojiya <suraj.kanojiya@biz2credit.org>
2018-08-22 18:08:33 +0530

    featured post created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { FeaturedComponent } from './featured/featured.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    FeaturedComponent
   ],
   imports: [
     BrowserModule
